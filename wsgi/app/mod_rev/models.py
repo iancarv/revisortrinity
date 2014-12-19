abeaba5000f1514bb3b9bbd994802b21d1c6d7d0
Ian Carvalho <iancarv@gmail.com>
2014-12-19 14:45:45 -0200

    adding data

---
+++ wsgi/app/mod_rev/models.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+Verbete(object):
+    __init__(self, name, desc, audio='', pronuncia='', sep=''):
+        self.name = name
+        self.desc = desc
+        self.audio = audio
+        self.pronuncia = pronuncia
+        self.sep = sep
--- wsgi/app/views.py
+++ wsgi/app/views.py
@@ -1,5 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+	env_var = os.environ['OPENSHIFTDATADIR']
+    f = open('/static/verbetes.json')
+
+
+@app.route('/verbetes/<verbete>')
+def show_user_profile(verbete):
+    return render_template('verbetes.html', verbete)
