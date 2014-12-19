744ce739e3102ef8a39e5aa42619479cfa155040
Ian Carvalho <iancarv@gmail.com>
2014-12-19 15:39:22 -0200

    working

--- wsgi/app/views.py
+++ wsgi/app/views.py
@@ -9,12 +9,11 @@ from pymongo import MongoClient
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello'
-    # env_uri = env_var = os.environ['OPENSHIFT_MONGODB_DB_URL']
-    # client = MongoClient(env_uri)
-    # db = client['trinity']
-    # collection = db['verbetes']
-    # return str(collections)
+    env_uri = env_var = os.environ['OPENSHIFT_MONGODB_DB_URL']
+    client = MongoClient(env_uri)
+    db = client['trinity']
+    collection = db['verbetes']
+    return str(collections)
 
 
 @app.route('/verbetes/<verbete>')
