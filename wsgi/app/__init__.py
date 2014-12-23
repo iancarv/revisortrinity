a58b6b37fe212d96cd18d155e50897a2ffdb2388
Ian Carvalho <iancarv@gmail.com>
2014-12-23 18:20:14 -0200

    link fix

--- wsgi/app/__init__.py
+++ wsgi/app/__init__.py
@@ -4,7 +4,7 @@ import os
 envs = {}
 envs['data'] = os.environ.get('OPENSHIFT_DATA_DIR') or '../../local_data'
 envs['db_url'] = os.environ.get('OPENSHIFT_MONGODB_DB_URL') or 'localhost:27017'
-app = Flask(__name__)
+app = Flask(__name__, static_folder='app/static')
 client = MongoClient(envs['db_url'])
 db = client['trinity']
 collection = db['verbetes']
